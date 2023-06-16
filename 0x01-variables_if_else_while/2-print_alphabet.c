3a544b043cec0e50e69eaf60a566e4a56c8d0d46
GOURCHANE YOUSSEF <contact.gourchane@gmail.com>
2023-06-16 14:52:08 -0700

    2-print_alphabets.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
