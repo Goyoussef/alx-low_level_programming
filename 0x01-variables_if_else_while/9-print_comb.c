fa1d597aec6a3cf131cbf92d24b024378e83f4b4
GOURCHANE YOUSSEF <contact.gourchane@gmail.com>
2023-06-16 16:22:56 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - main block
- * Description: prints all single digit numbers of base 16 
- * starting from 0, followed by a new line.
- * Return: 0
+ *main - main block
+ *prints all single digit numbers of base 16
+ *start from 0, followed by a comma and a space.
+ *Return: always  0
  */
 int main(void)
 {
-	int c = 0;
-
-	while (c < 10)
-	{
-		putchar(48 + c);
-		if (c != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		c++;
-	}
-	putchar('\n');
-	return (0);
+int n;
+for  (n = 48; n <= 57; n++)
+{
+{
+putchar(n);
+if (n == 57)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
 }
