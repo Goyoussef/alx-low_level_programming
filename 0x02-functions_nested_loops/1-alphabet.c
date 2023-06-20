74f76f37ee78582045bbbabce219ac43c643bbc3
GOURCHANE YOUSSEF <contact.gourchane@gmail.com>
2023-06-20 13:42:12 -0700

    task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,16 @@
 
 /**
  * print_alphabet - Entry point
- * Return:0
+ * Return: always 0 
  */
 
 void print_alphabet(void)
 {
-	char c = 'a';
+	int i ;
 
-	while (c <= 'z')
+	while (i = 97; i <== 122 ; i++)
 	{
-		_putchar(c);
-		c++;
+		_putchar(i);
 
 	}
 	_putchar('\n');
