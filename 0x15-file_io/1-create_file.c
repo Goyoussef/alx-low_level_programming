b8476b9ac1879e1a2fa226bd3e80e16e4c8a0819
GOURCHANE YOUSSEF <contact.gourchane@gmail.com>
2023-08-08 03:27:01 -0700

    fils 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include "main.h"
 
 /**
  * _strlen - compute the length of a NULL-terminated string
@@ -36,7 +36,7 @@ int create_file(const char *filename, char *text_content)
 
 	lf = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (lf< 0)
+	if (lf < 0)
 		return (-1);
 
 	if (text_content)
